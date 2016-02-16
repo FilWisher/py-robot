b8fb48eb52877719fa725b57d28d405c3f4879f1
FilWisher <williamfisher@hotmail.co.uk>
2016-02-16 11:39:52 +0000

    adds robot.py functions [WIP: still needs basic robot setup]

--- robot.py
+++ robot.py
@@ -1,1 +1,16 @@
-print 'hello world'
+# go forwards
+def forwards(cm):
+    angle = cm * 0.45
+    interface.increaseMotorAngleReferences(motors, [angle, angle])
+
+def backwards(cm):
+    angle = cm * 0.45
+    interface.increaseMotorAngleReferences(motors, [-angle, -angle])
+    
+def left(degree):
+    angle = cm * 0.17
+    interface.increaseMotorAngleReferences(motors, [angle, -angle])
+    
+def right(degree):
+    angle = cm * 0.17
+    interface.increaseMotorAngleReferences(motors, [-angle, angle])
