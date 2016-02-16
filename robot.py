9c98c68bbd7b3414a8d5d1f73c9fa45799f758fd
Luka Milic <lm1015@ic.ac.uk>
2016-02-16 11:47:19 +0000

    added header to robot.py

--- robot.py
+++ robot.py
@@ -1,3 +1,29 @@
+import brickpi
+import time
+
+interface=brickpi.Interface()
+interface.initialize()
+
+motors = [0,1]
+speed = 6.0
+
+interface.motorEnable(motors[0])
+interface.motorEnable(motors[1])
+
+motorParams = interface.MotorAngleControllerParameters()
+motorParams.maxRotationAcceleration = 6.0
+motorParams.maxRotationSpeed = 12.0
+motorParams.feedForwardGain = 255/20.0
+motorParams.minPWM = 18.0
+motorParams.pidParameters.minOutput = -255
+motorParams.pidParameters.maxOutput = 255
+motorParams.pidParameters.k_p = 100.0
+motorParams.pidParameters.k_i = 0.0
+motorParams.pidParameters.k_d = 0.0
+
+interface.setMotorAngleControllerParameters(motors[0],motorParams)
+interface.setMotorAngleControllerParameters(motors[1],motorParams)
+
 # go forwards
 def forwards(cm):
     angle = cm * 0.45
@@ -6,11 +32,11 @@ def forwards(cm):
 def backwards(cm):
     angle = cm * 0.45
     interface.increaseMotorAngleReferences(motors, [-angle, -angle])
-    
+
 def left(degree):
     angle = cm * 0.17
     interface.increaseMotorAngleReferences(motors, [angle, -angle])
-    
+
 def right(degree):
     angle = cm * 0.17
     interface.increaseMotorAngleReferences(motors, [-angle, angle])
