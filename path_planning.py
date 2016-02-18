6f0362b0de044d4669a0999556f085f16d7cd2b1
Luka Milic <lm1015@ic.ac.uk>
2016-02-18 19:11:19 +0000

    use numpy radian converter

--- path_planning.py
+++ path_planning.py
@@ -6,8 +6,8 @@ import robot
 def navigateToWaypoint(X,Y):
 
 
-    #assuming robot is at (0,0,0) 
-    
+    #assuming robot is at (0,0,0)
+
     #xnew = cos(theta)*d
     #ynew = sin(theta)*d
 
@@ -17,28 +17,29 @@ def navigateToWaypoint(X,Y):
 
     #theta is in radians
     theta_rad = np.arccos(cos_theta)
-    
+
     #turn theta into degrees
     theta_deg = theta_rad * 360.0 / (2.0* np.pi)
 
     #call function that rotates robot by theta_deg degrees
     robot.left(theta_deg)
-    
+
 
     #call function to move robot straight for d cm
     robot.forwards(d)
 
 
-
+path_planning((0,0,0),(0,-1))
+888
 def path_planning(current_loc, goal):
 
     #need to translate our frame of reference
     x_c, y_c, theta_c = current_loc  #!!!!!make sure that theta_c is in degrees!
     x_g, y_g = goal
 
-    
+
     d = np.sqrt((x_c-x_g)**2+(y_c-y_g)**2)
-    
+
 
     #calculate angle of goal point with respect to real frame
     sin_theta = y_g/d
@@ -46,16 +47,17 @@ def path_planning(current_loc, goal):
 
     #theta is in radians
     theta_rad = np.arccos(cos_theta)
-    
+
     #turn theta into degrees
-    theta_deg = theta_rad * 360 / 2* np.pi  
-    
+    theta_deg = np.rad2deg(theta_rad)
+
+
     #subtract the angle robot is facing  with respect to real frame from angle of goal point
     #to obtain abgle the robot needs to turn to
     #if this angle is negative, robot has to turn this amount right
     #if angle is positive, robot has to turn this amount left
     theta_to_move = theta_deg - theta_c
-    
+
     print theta_to_move
 
     if theta_to_move < 0:
@@ -63,24 +65,19 @@ def path_planning(current_loc, goal):
         print 'moving right'
         robot.right(int(theta_to_move))
 
-    else: 
+    else:
         print 'moving left'
         #turn left amount theta_to_move
         robot.left(int(theta_to_move))
-    
+
     #xnew = cos(theta)*d
     #ynew = sin(theta)*d
     X = abs(x_c - x_g)
     Y = abs(y_c - y_g)
     d = np.sqrt(X*X + Y*Y)
 
-    
+
     #after turned into correct direction, move d cm forward
     robot.forwards(d)
-    
+
 path_planning((0,0,0),(4,1))
-    
-    
-
-
-    
