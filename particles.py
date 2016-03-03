ae6689c071761043e2d825a81d2758c984e0493e
pi <pi@pi.pi>
2016-03-03 13:30:27 +0000

    moved good stuff from luka branch

--- particles.py
+++ particles.py
@@ -9,10 +9,31 @@ import sys
 #using straight rotational sigma of 0.2
 #using pure rotational sigma of 0.2
 
+def unit_vector(deg):
+  """
+  Returns unit vector in form (x, y) from angle in degrees
+  """
+  rad = np.deg2rad(deg)
+  return (np.cos(rad), np.sin(rad))
+
+def mean_angle(particles):
+  """
+  Averages angles via unit vector conversions to circumvent
+  weirdness of averageing 360 with 0
+  """
+  angles = map(lambda x: x[2], particles)
+  sum = [0, 0]
+  uvectors = map(unit_vector, angles)
+  for uvector in uvectors:
+    sum[0] += uvector[0]
+    sum[1] += uvector[1]
+  average = [float(sum[0])/float(len(uvectors)), float(sum[1])/float(len(uvectors))]
+  return np.rad2deg(np.arctan2(average[1], average[0]))
+
 
 class Particles:
     def __init__(self, initial_location = (1,1,0), N=100):
-        self.particles    = []
+        self.data    = []
         self.weights      = []
         self.safe_to_update = []
         self.nearest_wall = []
@@ -20,7 +41,7 @@ class Particles:
         self.sigma = 1.0
 
         for i in xrange(0, N):
-            self.particles.append((initial_location[0], initial_location[1], initial_location[2]))
+            self.data.append((initial_location[0], initial_location[1], initial_location[2]))
             self.weights.append(1.0/float(N))
             self.safe_to_update.append(True)
             self.nearest_wall.append(-1)
@@ -30,46 +51,41 @@ class Particles:
         xSum = 0
         ySum = 0
         thetaSum = 0
-        numberOfTuples = len(self.particles)
-        for particle in self.particles:
+        numberOfTuples = len(self.data)
+        for particle in self.data:
             x, y, theta = particle
             xSum += x
             ySum += y
             thetaSum += theta
 
-        return (xSum/numberOfTuples, ySum/numberOfTuples, thetaSum/numberOfTuples)
+        return (xSum/numberOfTuples, ySum/numberOfTuples, mean_angle(self.data))
 
-
-    #function to find the nearest wall and the distance to it for ALL particles
     def update_nearest_walls(self,walls):
         self.nearest_wall = []
-	self.distance_to_nearest_wall = []
-        for i in xrange(len(self.particles)):
-            wall_index, distance = self.get_wall_info(self.particles[i], walls)
+        self.distance_to_nearest_wall = []
+        for i in xrange(len(self.data)):
+            wall_index, distance = self.get_distance_to_nearest_wall(self.data[i], walls)
             self.nearest_wall.append(wall_index)
             self.distance_to_nearest_wall.append(distance)
 
-    #function to find the nearest wall and distance to it for ONE particle
-    def get_wall_info(self, particle, walls):
-
-        #NOTE that these values will be returned if no wall is found for particle
+    def get_distance_to_nearest_wall(self, particle, walls):
         nearest_wall_distance = 2000
         nearest_wall_index = -1
-
         for i in xrange(len(walls)):
             distance_from_wall = self.calculate_distance_from_wall(particle, walls[i])
-            #check if the particle faces the wall
-	    if (self.check_particle_faces_wall(particle, walls[i], distance_from_wall)):
+            ok = self.check_particle_faces_wall(particle, walls[i], distance_from_wall)
+            if (ok):
                 # If we find a smaller distance set it
                 if(nearest_wall_distance > distance_from_wall):
                     nearest_wall_distance = distance_from_wall
                     nearest_wall_index = i
 
+        # if nearest_wall_distance > sys.maxint - 40000:
+        #     print particle
         return ( nearest_wall_index, nearest_wall_distance )
 
 
-    #function to calculate the orthogonal distance from a particle to a wall
-    def orthogonal_distance(self,particle,wall):
+    def calculate_shortest_distance_from_wall(self,particle,wall):
         # Used https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
         x0, y0, theta = particle
         theta_rad = np.deg2rad(theta)
@@ -81,22 +97,20 @@ class Particles:
             return -1
         return numerator/denominator
 
-    #function to check that the measurement for ONE particle is safe
     def check_measurement_is_safe(self,particle_index,walls):
         nearest_wall_index = self.nearest_wall[particle_index]
-        #measurement is not safe if there is no nearest wall
-	if nearest_wall_index == -1:
-		return False
+        if nearest_wall_index == -1:
+                return False
         distance_to_wall = self.distance_to_nearest_wall[particle_index]
-        short_distance_to_wall = self.orthogonal_distance(self.particles[particle_index], walls[nearest_wall_index])
+        short_distance_to_wall = self.calculate_shortest_distance_from_wall(self.data[particle_index], walls[nearest_wall_index])
         angle_with_wall = np.arccos(short_distance_to_wall/distance_to_wall)
-        
+        #error measurements indicated that at an angle of 40 degrees from point to walls
+        #measurements become unreliable. Used 180 - 90 - 40 = 50 degrees:
         if(np.rad2deg(angle_with_wall) > 40):
             return False
         else:
             return True
 
-    #function to calculate the distance of ONE particle to a given wall
     def calculate_distance_from_wall(self,particle, wall):
         x, y, theta = particle
         theta_rad = np.deg2rad(theta)
@@ -108,7 +122,6 @@ class Particles:
             return -sys.maxint
         return numerator/denominator
 
-    #function which checks if x is in between x1 and x2
     def in_range(self,x, a, b):
         ok1 = a <= x <= b
         ok2 = a >= x >= b
@@ -117,7 +130,6 @@ class Particles:
         else:
             return False
 
-    #function which checks if ONE particle faces a given wall - essentially if it can intersect with the wall
     def check_particle_faces_wall(self,particle,wall,m):
         """
         Note: edges cases like facing the corner of a wall return false
@@ -133,78 +145,31 @@ class Particles:
         else:
             return False
 
-    #function determine if an update using mcl is safe, according to particle cloud
     def update_is_safe(self, walls):
         self.safe_to_update = []
         trues = 0
-        for i in xrange(len(self.particles)):
+        for i in xrange(len(self.data)):
             self.safe_to_update.append(self.check_measurement_is_safe(i, walls))
         for i in xrange(len(self.safe_to_update)):
             if self.safe_to_update[i]:
                 trues += 1
-        if trues > len(self.particles)/2:
+        if trues > len(self.data)/2:
+            print 'ITS SAFE: ', trues, '%'
             return True
         else:
             print 'NOT SAFE', trues, '%'
             return False
 
 
-
-#MAIN FUNCTIONS FOR MCL ALGORITHM#
-
-    #function which updates weights
     def update_weights(self,walls,measurement):
-        for i in xrange(len(self.particles)):
+        for i in xrange(len(self.data)):
             #make sure that the distance_to_nearest wall is not 2000 (the default value)
-            if distance_to_nearest_wall[i] != 2000:
+            if self.distance_to_nearest_wall[i] != 2000:
                 difference = self.distance_to_nearest_wall[i] - measurement
-                likelihood = np.exp(-((difference)**2)/(2*self.sigma*self.sigma))
+                arg = -((float(difference))**2)/(2*self.sigma**2)
+                likelihood = np.exp(arg)
                 self.weights[i] = self.weights[i]*likelihood
-            #otherwise, don't update the weight, leave it as it is
 
-
-
-    #function that normalises the weights of the particle cloud
-    def normalize(self):
-        sum = 0.0;
-        for weight in self.weights:
-            sum += weight
-        if(sum != 0):
-            for i in xrange(len(self.particles)):
-                self.weights[i] = self.weights[i]/sum
-        else:
-            print "Warning! Particle weights got really small!"
-            for i in xrange(len(self.particles)):
-                self.weights[i] = 1.0/float(len(self.particles))
-
-    #function which resamples using cumulative probability distribution
-    def resample(self):
-        """
-        Resample N new particles with weight calculated using mcl with distribution
-        determined by weight of particles
-        """
-        new_particles = []
-        particles_count = len(self.particles)
-        for i in xrange(particles_count):
-            r = random.random()
-            sum = 0
-            for i in xrange(len(self.weights)):
-                sum += self.weights[i]
-                if (sum >= r):
-                    #append particle for which r lies within its weight 
-                    new_particles.append(self.particles[i])
-                    break
-
-        #reset the weights to (normalized) equal number
-        for i in xrange(0,particles_count):
-            self.weights[i] = 1.0/float(particles_count)
-
-        self.particles = new_particles
-
- ###########END OF HELPER FUNCYIONS###############
-
-
- #MCL ALGORITHM
     def do_mcl(self,walls,measurement):
         #for each particle, finds the nearest wall and the distance to it.
         self.update_nearest_walls(walls)
@@ -215,59 +180,53 @@ class Particles:
             self.resample()
 
 
-#Movement functions for the particles
+    def normalize(self):
+        sum = 0.0;
+        for weight in self.weights:
+            sum += weight
+        if(sum != 0):
+            for i in xrange(len(self.data)):
+                self.weights[i] = self.weights[i]/sum
+        else:
+            print "Warning! Particle weights got really small!"
+            for i in xrange(len(self.data)):
+                self.weights[i] = 1.0/float(len(self.data))
+
+    def resample(self):
+        """
+        Resample N new particles with weight 1/N with distribution
+        determined by weight of particles
+        """
+        new_particles = []
+        particles_count = len(self.data)
+        for i in xrange(particles_count):
+            r = random.random()
+            sum = 0
+            for i in xrange(len(self.weights)):
+                sum += self.weights[i]
+                if (sum >= r):
+                    new_particles.append(self.data[i])
+                    break
+
+        for i in xrange(0,particles_count):
+            self.weights[i] = 1.0/float(particles_count)
+
+        self.data = new_particles
 
     def left(self, deg):
-        for i in range(len(self.particles)):
-            x,y,theta = self.particles[i]
-            g = random.gauss(0,0.2)
-            #TODO: Check how the adding of deg and g affects mean angle, would it cause problems?
-            self.particles[i] = (x, y, theta + deg + g)
+        for i in range(len(self.data)):
+            x,y,theta = self.data[i]
+            g = random.gauss(0,0.1)
+            self.data[i] = (x, y, theta + deg + g)
 
     def forwards(self, cm):
         #update particle position after moving cm distance
-        for i in range(len(self.particles)):
-                x,y,theta = self.particles[i]
-                e = random.gauss(0,1.6)
-                f = random.gauss(0,0.2)
+        for i in range(len(self.data)):
+                x,y,theta = self.data[i]
+                e = random.gauss(0,0.5)
+                f = random.gauss(0,0.1)
 
-                self.particles[i] = ((x + (cm + e)*np.cos(np.deg2rad(theta))), (y + (cm + e)*np.sin(np.deg2rad(theta))), (theta + f))
-
-
+                self.data[i] = ((x + (cm + e)*np.cos(np.deg2rad(theta))), (y + (cm + e)*np.sin(np.deg2rad(theta))), (theta + f))
     def getFakeSensorMeasurement(self,walls):
-        i, d = self.get_wall_info(self.mean(),walls)
+        i, d = self.get_distance_to_nearest_wall(self.mean(),walls)
         return d+random.gauss(0.0,1.0)
-
-"""
-    TESTING
-"""
-"""
-test = Particles()
-# print test.temp((0,0,45,0),(-10,10,10,10))
-
-test.particles[0] = (5,5,0,0.01)
-
-print test.get_distance_to_nearest_wall(test.particles[0], walls)
-test.left(45)
-test.update_weights(walls, 7.07106)
-print "UPDATED"
-print test.particles
-test.normalize()
-print "NORMALIZED"
-print test.particles
-
-sum = 0
-for particle in test.particles:
-  sum += particle[3]
-print "sum: ", sum
-
-test.resample()
-print "RESAMPLED"
-print test.particles
-
-print test.particles
-print test.get_distance_to_nearest_wall(test.particles[0], walls)
-test.forwards(2)
-print test.particles
-print test.get_distance_to_nearest_wall(test.particles[0], walls)
-"""
