ae05c3a863b4a8df21c32623de9b0ccbe06cbb8b
Luka Milic <lukam321@gmail.com>
2016-03-05 22:00:03 +0000

    commented run.py

--- run.py
+++ run.py
@@ -31,18 +31,28 @@ if run_option == 2:
 ######################################################  
 if run_option == 3:
 
+  # Use this function to reshuffle the waypoints depending on where you find yourself
   def shift(seq, n):
       n = n % len(seq)
       return seq[n:] + seq[:n]
 
+  # The angle at which the robot is started
   start_angle = 100
+  # The waypoint index at which the robot starts at
+  waypoint_idx = 3
+  x,y = waypoints_cw3[waypoint_idx]
+  # Create a recognition class
   rec = Recognition(False,waypoints_cw4,start_angle)
-  x,y = waypoints_cw3[3]
-  res = rec.sim_recognise(x,y,angle)
-  print 'Expected ', (3,angle), ' got ', res
+  # Try and recognise the waypoint
+  result = rec.sim_recognise(x,y,angle)
+  print 'Expected ', (waypoint_idx,angle), ' got ', result
   
+  # Shift the waypoints to account for the starting position
   waypoints = shift(waypoints_cw4,res[0]+1)
+  # Number of particles for mcl
   noParticles = 100
-  nav = WaypointNavigation(use_robot,waypoints,res[0],noParticles)
+  # Create a navigation class
+  nav = WaypointNavigation(use_robot,waypoints,result[0],noParticles)
+  # Navigate
   nav.navigate()
 
